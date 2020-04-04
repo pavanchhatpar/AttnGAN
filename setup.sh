a80a22102ad807210cae81a0ac9d73897f695f91
pavanchhatpar <pavanchhatpar@gmail.com>
2020-04-03 20:45:14 -0400

    fix setup order

--- setup.sh
+++ setup.sh
@@ -1,7 +1,3 @@
-pip install python-dateutil easydict pandas torchfile nltk scikit-image
-pip install http://download.pytorch.org/whl/cu90/torch-0.3.1-cp27-cp27mu-linux_x86_64.whl
-pip install torchvision==0.2.1
-
 if [ ! -f "./.env" ]; then
     echo "Make a file `.env` and add content in it as shown in the sample below"
     echo ""
@@ -9,6 +5,10 @@ if [ ! -f "./.env" ]; then
     echo "export DAMSMBIRD=\"./drive/My Drive/Spring 20/Spec. AI/bird.zip\""
     echo "export GANBIRD=\"./drive/My Drive/Spring 20/Spec. AI/bird_AttnGAN2.pth\""
     exit(2)
+
+pip install python-dateutil easydict pandas torchfile nltk scikit-image
+pip install http://download.pytorch.org/whl/cu90/torch-0.3.1-cp27-cp27mu-linux_x86_64.whl
+pip install torchvision==0.2.1
 
 source "./.env"
 
